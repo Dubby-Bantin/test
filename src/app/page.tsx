7e24bca9aa64493e37e509fb9e67f0e251ffe9ca
Dubby-Bantin <dubbybantin@icloud.com>
2025-03-08 11:16:39 -0500

    updated page file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import JobApplication from "@/components/job_application";
 
 export default function Home() {
-  return <JobApplication />;
+  return (
+    <div className="flex items-center w-full justify-center p-20">
+      <JobApplication />
+    </div>
+  );
 }
